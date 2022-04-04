610af130e73dbb5f709c6adf7f8a0c1bb104322a
tomas <ruzicka.tomas93@gmail.com>
2022-04-04 16:51:23 +0200

    priprava REST api

---
+++ src/main/java/cz/project/recepty/rest/RestApplication.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cz.project.recepty.rest;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author tomas
+ */
+
+@ApplicationPath("/rest/v1")
+public class RestApplication extends Application {
+    
+    	private Set<Class<?>> empty = new HashSet<>();
+        
+        public RestApplication() {
+            empty.add(ReceptyResource.class);
+        }
+}
